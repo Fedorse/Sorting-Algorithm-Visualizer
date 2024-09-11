996d14ef034d4afa7d2222601dbecd23f0c00abd
Fedor_Semochkin <221800ss@gmail.com>
2024-09-11 12:47:48 +0700

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
+
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />,
+  <>
+    <Analytics />,
+    <App />,
+  </>,
 );
